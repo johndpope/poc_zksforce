b51c4a7601d6c7cd6f9c50a4c690f525a7f27de0
Asif Junaid <asif.junaid@nsiglobal.com>
2017-01-17 10:26:27 +0400

    zkforce init

--- poc_zksforce/ViewController.swift
+++ poc_zksforce/ViewController.swift
@@ -7,14 +7,24 @@
 //
 
 import UIKit
-
+import ZKSforce
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+    }
+    
+    func loginToSalesforce(){
+        var result : ZKLoginResult?
+        do {
+            let sforce : ZKSforceClient = try ZKSforceClient()
+            
+        }catch{
+            print("error")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
