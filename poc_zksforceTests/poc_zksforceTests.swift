5fe6b0d2aaaf715f54f63719417155e5a9a8e658
Asif Junaid <asif.junaid@nsiglobal.com>
2017-01-17 09:57:02 +0400

    init

---
+++ poc_zksforceTests/poc_zksforceTests.swift
@@ -0,0 +1,36 @@
+//
+//  poc_zksforceTests.swift
+//  poc_zksforceTests
+//
+//  Created by Asif Junaid on 17/01/2017.
+//  Copyright © 2017 Asif Junaid. All rights reserved.
+//
+
+import XCTest
+@testable import poc_zksforce
+
+class poc_zksforceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
